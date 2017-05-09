c3e8a545a3988d2268876f72aa453dfeb824b4b5
Rogerio Chaves <rogeriocfj@gmail.com>
2017-05-09 18:39:56 -0300

    Use babel to parse AST and generate source-maps

--- index.js
+++ index.js
@@ -29,7 +29,15 @@
  *
  */
 
-module.exports = function(rawSource) {
+var babel = require('babel-core');
+var transform = babel.transform;
+var traverse = babel.traverse;
+var types = babel.types;
+var transformFromAst = babel.transformFromAst;
+var template = babel.template;
+var injectorTemplate = require('./injectorTemplate');
+
+module.exports = function(rawSource, inputSourceMap) {
 
     if (this && this.cacheable) this.cacheable();
 
@@ -37,69 +45,37 @@ module.exports = function(rawSource) {
     // all dependencies
     var DEFINE_REGEX = /define\(\[[\s\S]*?\)/;
 
-
-    // this function will be returned by the mocked module
-    // and make sure we're able to mock dependencies by passing an
-    // object with dependency paths as keys and mocks as values
-    var injector = function (mocks) {
-
-        // Create a custom define method which the original module will
-        // use to resolve dependencies. If a mock is passed for a given path
-        // the mock will be used, otherwise the original dependency will be
-        // loaded.
-        var __define__ = function (paths, definition) {
-
-            var deps = [];
-
-            function relsoveDependency (path, index) {
-
-                index = index || 0;
-
-                if(mocks && mocks[path]) {
-                    deps.push(mocks[path])
-                } else {
-                    deps.push(originalDeps[index]);
-                }
-            }
-
-            if (paths) {
-                if(typeof paths === 'string') {
-                    relsoveDependency(paths);
-                } else {
-                    paths.forEach(relsoveDependency);
-                }
-            }
-
-            return definition.apply(this, deps);
-        }
-
-        return (function () {
-            __SOURCE_PLACEHOLDER__
-        })();
-    };
-
-    var output = [];
-
     // replace define() with __define__ in the original source so that our
     // altered __define__() method gets called
-    var source = rawSource.replace('define', 'return __define__');
+    var source = rawSource.replace('define', '__define__');
+    // get the original dependencies so that we can access them from the injector
+    var originalDefine = rawSource.match(DEFINE_REGEX)[0];
 
-    // convert the injector to a string and insert module source code
-    var injectorString = injector.toString().replace('__SOURCE_PLACEHOLDER__', source);
+    // the file should be a valid javascript before babel template can read it,
+    // so we replace original define directly on the string
+    var injector = injectorTemplate.replace('ORIGINAL_DEFINE', originalDefine);
 
-    // start with the original define block in order to retrieve all
-    // original dependencies
-    output.push(rawSource.match(DEFINE_REGEX)[0] + '{');
+    var ast = transform(source, {
+      babelrc: false,
+      code: false,
+      compact: false,
+      filename: this.resourcePath,
+    }).ast;
 
-    // write original dependencies to an array so that we can access them from
-    // the injector
-    output.push('var originalDeps = arguments;');
+    var wrapperModuleAst = types.file(types.program([
+      template(injector)({
+        SOURCE: ast
+      }),
+    ]));
 
-    // add the module wrapped and altered to worked with our injector
-    output.push('return ' + injectorString);
+    var transformed = transformFromAst(wrapperModuleAst, source, {
+      sourceMaps: this.sourceMap,
+      sourceFileName: this.resourcePath,
+      inputSourceMap,
+      babelrc: false,
+      compact: false,
+      filename: this.resourcePath,
+    });
 
-    // close define block
-    output.push('});');
-
-    return output.join('\n');
+    this.callback(null, transformed.code, transformed.map);
 };
